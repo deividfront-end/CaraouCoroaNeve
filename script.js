350e40800469d61c8a4f58865ebb6ac197aa2fb0
Deivid de Souza <101485535+deividfront-end@users.noreply.github.com>
2025-03-02 19:30:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const moeda = document.getElementById('moeda');
 const girarBotao = document.getElementById('girar');
-const resultadoDiv = document.getElementById('resultado'); // Obtendo a div de resultado
+const resultadoDiv = document.getElementById('resultado');
 
 girarBotao.addEventListener('click', () => {
-    const resultado = Math.random() < 0.5 ? 0 : 180;
+    const resultado = Math.random() < 0.5 ? 'cara' : 'coroa';
     const rotacaoAleatoria = Math.random() * 7200 + 3600;
+    const rotacaoFinal = resultado === 'cara' ? rotacaoAleatoria : rotacaoAleatoria + 180;
+
+    girarBotao.disabled = true; // Desabilita o botão
+
     moeda.style.transition = 'transform 4s ease-in-out';
-    moeda.style.transform = `rotateY(${rotacaoAleatoria + resultado}deg)`;
+    moeda.style.transform = `rotateY(${rotacaoFinal}deg)`;
+
     setTimeout(() => {
         moeda.style.transition = 'transform 0.3s ease-in-out';
-        moeda.style.transform = `rotateY(${resultado}deg)`;
-        if (resultado === 0) {
-            resultadoDiv.textContent = 'Deu STALKER!'; // Exibindo a mensagem de Cara
-        } else {
-            resultadoDiv.textContent = 'Deu SKYRIM!'; // Exibindo a mensagem de Coroa
-        }
+        moeda.style.transform = `rotateY(${resultado === 'cara' ? 0 : 180}deg)`;
+        resultadoDiv.textContent = `Deu ${resultado === 'cara' ? 'SKYRIM' : 'STALKER'}!`;
+        girarBotao.disabled = false; // Reabilita o botão
     }, 4000);
 });
